a88997ab93a06228255af42a4c02d8e63f984b6b
Karan <Karansj4723@gmail.com>
2025-03-21 06:04:25 +0530

    Cors added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # 👈 Import CORS
 
 app = Flask(__name__)
+CORS(app)  # 👈 Enable CORS for all routes
 
 # Sample data
 items = [
