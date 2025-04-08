90c8249a8863986c06d235774a7d67b78b6bae19
Karan <Karansj4723@gmail.com>
2025-04-08 07:32:09 +0530

    updated playlist module

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # 👈 Import CORS
 from dotenv import load_dotenv
+import requests
 import os
 
 load_dotenv()
@@ -11,10 +12,63 @@ CORS(app)  # 👈 Enable CORS for all routes
 # API's for various applications
 
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/playlistItems"
+
+def fetch_playlist_videos(api_key, playlist_id):
+    videos = []
+    next_page_token = ""
+
+    while True:
+        params = {
+            "part": "snippet",
+            "playlistId": playlist_id,
+            "maxResults": 50,
+            "pageToken": next_page_token,
+            "key": api_key
+        }
+
+        response = requests.get(YOUTUBE_API_URL, params=params)
+        data = response.json()
+
+        if "error" in data:
+            return {"error": data["error"]["message"]}
+
+        for item in data["items"]:
+            snippet = item["snippet"]
+            video = {
+                "title": snippet["title"],
+                "description": snippet["description"],
+                "videoId": snippet["resourceId"]["videoId"],
+                "thumbnail": snippet["thumbnails"]["medium"]["url"],
+                "publishedAt": snippet["publishedAt"],
+                "channelTitle": snippet["channelTitle"]
+            }
+            videos.append(video)
+
+        next_page_token = data.get("nextPageToken")
+        if not next_page_token:
+            break
+
+    return videos
+
 
 @app.route("/")
 def home():
     return "Hello world"
+
+@app.route('/api/playlist/aptitude', methods=['GET'])
+def get_playlist_videos():
+    playlist_id = "PLAEwcO9ReeIG0BrjkUH1_q3GIY2m5jUYS"
+    if not playlist_id:
+        return jsonify({"error": "Missing 'playlistId' query parameter"}), 400
+
+    result = fetch_playlist_videos(YOUTUBE_API_KEY, playlist_id)
+
+    if isinstance(result, dict) and result.get("error"):
+        return jsonify(result), 500
+
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
